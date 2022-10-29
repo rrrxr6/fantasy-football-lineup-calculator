9641155db048fe6f9ce47cb14d11ab089d8ccbbb
Ryan Roach <ryan.roach@cerner.com>
2022-10-29 01:28:14 -0500

    scrape ownership

---
+++ src/main/java/roach/ryan/ff/data/scrape/FanDuelOwnershipRetriever.java
@@ -0,0 +1,42 @@
+package roach.ryan.ff.data.scrape;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import roach.ryan.ff.model.PlayerBuilder;
+
+public class FanDuelOwnershipRetriever
+{
+    private static final String OWNERSHIP_URL = "https://www.pff.com/dfs/ownership";
+
+    public Map<PlayerKey, PlayerBuilder> retrieve(Map<PlayerKey, PlayerBuilder> playerData)
+    {
+        try
+        {
+            Document doc = Jsoup.connect(OWNERSHIP_URL).get();
+            Elements tables = doc.select(".dfs-ownership table");
+            Element fanDuelTable = tables.get(0);
+            Elements rows = fanDuelTable.select("tbody tr");
+            for (Element row : rows)
+            {
+                Elements cols = row.select("td");
+                String name = cols.get(1).text();
+                String position = cols.get(4).text();
+                double ownership = Double.valueOf(cols.get(6).text().replace("%",""));
+                PlayerKey playerKey = new PlayerKey(name, position);
+                playerData.computeIfPresent(playerKey, (key, builder) -> builder.withOwnership(ownership));
+            }
+
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred while retrieving ownership data.");
+        }
+        return playerData;
+    }
+}
